05f06ef4aa741828ad38e7be2282d96069a10ac2
Nicholas Smith <nicholassmithbus@gmail.com>
2020-10-31 21:32:54 -0600

    cleaned up tweets.js

--- app/paths/tweets.js
+++ app/paths/tweets.js
@@ -26,23 +26,7 @@ app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
-// get a user by id
-app.get('/users/:id', async (req, res) => {
-    try {
-        const doc = await client.query(
-            Get(
-                Ref(
-                    Collection('users'),
-                    req.params.id
-                )
-            )
-        )
 
-        res.send(doc);
-    } catch(err) {
-        res.send(err);
-    }
-})
 
 // display form to post a new tweet:
 app.get('/newtweet', async (req, res) => {
@@ -68,8 +52,6 @@ app.get('/newtweet', async (req, res) => {
             users.push(docs.data[i].data.name);
         }
 
-        
-        // res.send(users);
         res.render("newtweet", {users: users});
 
     } catch(err) {  
@@ -82,7 +64,7 @@ app.get('/newuser', (req, res) => {
     res.render('newuser', {err: null});
 });
 
-// Create the new user in the database:
+// Create a new user in the database:
 app.post('/newuser', async (req, res) => {
     try {
         const docs = await client.query(
@@ -155,106 +137,6 @@ app.post('/tweet', async (req, res) => {
 
 });
 
-// Read a tweet by id:
-app.get('/tweet/:id', async (req, res) => {
-    
-    const doc = await client.query(
-        Get(
-            Ref(
-                Collection('tweets'),
-                req.params.id
-            )
-        )
-    )
-
-    res.send(doc);
-
-});
-
-// Query a user's tweets:
-app.get('/tweet', async (req, res) => {
-
-    const docs = await client.query(
-        Paginate(
-            Match(
-                Index('tweets_by_user'),
-                Call(Fn("getUser"), 'fireship_dev')
-            )
-        )
-    )
-
-    res.send(docs);
-
-});
-
-// create a relationship between two users (follower / followee)
-app.post('/relationship', async(req, res) => {
-
-    const data = {
-        follower: Call(Fn("getUser"), 'fireship_dev'),
-        followee: Call(Fn("getUser"), 'bob')
-    }
-
-    try{
-        const doc = await client.query(
-            Create(
-                Collection('relationships'),
-                { data }
-            )
-        )
-        
-        res.send(doc);
-
-    } catch(err) {
-        res.send(err);
-    }
-    
-});
-
-// Query a user's feed of tweets: 
-app.get('/feed', async (req, res) => {
-    try {
-        const docs = await client.query(
-            Paginate(
-                Join(
-                    
-                    Match( 
-                        Index('followees_by_follower'),
-                        Call(Fn("getUser"), "fireship_dev")
-                    ),
-                    Index('tweets_by_user'),
-                    
-                    
-                )
-            )
-        )
-        
-        let tweets = [];
-        for(let i = 0; i < docs.data.length; i++) {
-            let user = await client.query(
-                Get(
-                    Ref(
-                        Collection('users'),
-                        docs.data[i][0].id
-                    )
-                )
-            );
-
-            let data = {
-                user: user.data.name,
-                text: docs.data[i][1],
-            }
-
-            tweets.push(data);
-        }
-
-        res.send(tweets);
-
-    } catch(err) {
-        res.send(err);
-    }
-
-});
 
 // Query all tweets: 
 app.get('/all', async (req, res) => {
