cdaad1251bc155f09415188e97dd0216d2902b22
Nicholas Smith <nicholassmithbus@gmail.com>
2020-10-31 21:35:05 -0600

    updated port in index.js

--- app/index.js
+++ app/index.js
@@ -11,4 +11,6 @@ app.set('view engine', 'ejs');
 app.use(require('body-parser').urlencoded({extended: true}));
 app.use("/", allPaths);
 
-app.listen(5000, () => console.log('App on http://localhost:5000'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('App on http://localhost:5000'));
